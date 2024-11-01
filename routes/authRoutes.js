11972f31584c754077bd36e4f74c88a1373ef2d9
roshanbachhav <bachhavroshan600@gmail.com>
2024-11-01 22:14:28 +0530

    auth uri change t2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const authController = require('../controllers/authController');
+const express = require("express");
+const authController = require("../controllers/authController");
 const router = express.Router();
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 // router.get('/auth', authController.startOAuth);
 // router.get('/auth/callback', authController.handleOAuthCallback);
 // router.get('/callback', authController.callback);
 // app.use('/callback', authController.callback);
 // app.use('/accesstoken', authController.getAccessToken);
 
-
 // Define routes for OAuth
-router.get('/auth', authController.getAuthorizationUrl);
-router.get('/auth/callback', authController.handleCallback);
+// router.get('/auth', authController.getAuthorizationUrl);
+router.get("/auth", authController.startOAuth);
+router.get("/auth/callback", authController.handleCallback);
 
 module.exports = router;
-
-
-
