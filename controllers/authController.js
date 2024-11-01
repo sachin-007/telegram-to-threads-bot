ad2fcafefdea650d2e3cce806a498506cd98c533
roshanbachhav <bachhavroshan600@gmail.com>
2024-11-01 22:08:11 +0530

    auth uri change t1

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,75 +1,90 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const axios = require('axios');
-const AdminUser = require('../models/adminUser');
-const logActivity = require('../logActivity');
-require('dotenv').config();
-
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const AdminUser = require("../models/adminUser");
+const logActivity = require("../logActivity");
+require("dotenv").config();
 
 exports.register = async (req, res) => {
-    const { username, name, email, password } = req.body;
-  
-    try {
-      // Check if the user already exists
-      let user = await AdminUser.findOne({ email });
-      if (user) {
-        return res.status(400).json({ message: 'User already exists' });
-      }
-  
-      // Create a new user with hashed password
-      const hashedPassword = await bcrypt.hash(password, 10);
-      user = new AdminUser({ username, name, email, password: hashedPassword });
-      await user.save();
-  
-      // Generate JWT
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '12h' });
-      logActivity(`Barer token token for ${req.body.email} `+token);
+  const { username, name, email, password } = req.body;
 
-      
-      res.status(201).json({ token, message: 'Registration successful' });
-    } catch (error) {
-      await logActivity('Error registering user', { error: error.message });
-      res.status(500).json({ message: 'Error registering user', error: error.message });
+  try {
+    // Check if the user already exists
+    let user = await AdminUser.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
     }
-  };
-  
 
-  exports.login = async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      // Find the user by email
-      const user = await AdminUser.findOne({ email });
-      if (!user) {
-        return res.status(400).json({ message: 'Invalid credentials' });
-      }
-  
-      // Compare the password
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid credentials' });
-      }
-  
-      // Generate JWT
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '12h' });
-      logActivity(`login access token token for ${req.body.email} `+token);
-      res.status(200).json({ token, message: 'Login successful' });
-    } catch (error) {
-      await logActivity('Error logging in', { error: error.message });
-      res.status(500).json({ message: 'Error logging in', error: error.message });
+    // Create a new user with hashed password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = new AdminUser({ username, name, email, password: hashedPassword });
+    await user.save();
+
+    // Generate JWT
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "12h",
+    });
+    logActivity(`Barer token token for ${req.body.email} ` + token);
+
+    res.status(201).json({ token, message: "Registration successful" });
+  } catch (error) {
+    await logActivity("Error registering user", { error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    const user = await AdminUser.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
     }
-  };
-  
-  const scope = 'threads_basic,threads_content_publish';
-const THREAD_APP_ID=process.env.THREAD_APP_ID;
-const REDIRECT_URI=process.env.REDIRECT_URI;
-const THREADS_APP_SECRET=process.env.THREADS_APP_SECRET;
 
-// Step 1: Redirect to Authorization URL
-exports.getAuthorizationUrl = (req, res) => {
-  const authUrl = `https://threads.net/oauth/authorize?client_id=${THREAD_APP_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(scope)}&response_type=code`;
-  logActivity(`Generated Authorization URL: ${authUrl}`); // Log the authorization URL
+    // Compare the password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Generate JWT
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "12h",
+    });
+    logActivity(`login access token token for ${req.body.email} ` + token);
+    res.status(200).json({ token, message: "Login successful" });
+  } catch (error) {
+    await logActivity("Error logging in", { error: error.message });
+    res.status(500).json({ message: "Error logging in", error: error.message });
+  }
+};
+
+const scope =
+  "threads_basic,threads_content_publish,threads_manage_insights,threads_manage_replies,threads_read_replies";
+const THREAD_APP_ID = process.env.THREAD_APP_ID;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const THREADS_APP_SECRET = process.env.THREADS_APP_SECRET;
+const forceReauth = true;
+
+// // Step 1: Redirect to Authorization URL
+// exports.getAuthorizationUrl = (req, res) => {
+//   const authUrl = `https://threads.net/oauth/authorize?client_id=${THREAD_APP_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(scope)}&response_type=code`;
+//   logActivity(`Generated Authorization URL: ${authUrl}`); // Log the authorization URL
+//   res.redirect(authUrl);
+// };
+
+// Start the OAuth authorization process
+exports.startOAuth = (req, res) => {
+  const authUrl = `https://www.threads.net/oauth/authorize/?redirect_uri=${encodeURIComponent(
+    REDIRECT_URI
+  )}&client_id=${THREAD_APP_ID}&response_type=code&force_reauth=${forceReauth}&scope=${encodeURIComponent(
+    scope
+  )}`;
+
   res.redirect(authUrl);
 };
 
@@ -78,33 +93,38 @@ exports.handleCallback = async (req, res) => {
   const { code } = req.query;
 
   if (!code) {
-      logActivity('Authorization code not provided.'); // Log the error
-      return res.status(400).send('No authorization code provided.');
+    logActivity("Authorization code not provided."); // Log the error
+    return res.status(400).send("No authorization code provided.");
   }
 
   try {
-      const response = await axios.post('https://graph.threads.net/oauth/access_token', null, {
-          params: {
-              client_id: THREAD_APP_ID,
-              client_secret: config.THREADS_APP_SECRET,
-              grant_type: 'authorization_code',
-              redirect_uri: REDIRECT_URI,
-              code,
-          },
-      });
+    const response = await axios.post(
+      "https://graph.threads.net/oauth/access_token",
+      null,
+      {
+        params: {
+          client_id: THREAD_APP_ID,
+          client_secret: config.THREADS_APP_SECRET,
+          grant_type: "authorization_code",
+          redirect_uri: REDIRECT_URI,
+          code,
+        },
+      }
+    );
 
-      const { access_token, user_id } = response.data;
-      logActivity(`Successfully exchanged code for token. User ID: ${user_id}, Access Token: ${access_token}`); // Log the success
+    const { access_token, user_id } = response.data;
+    logActivity(
+      `Successfully exchanged code for token. User ID: ${user_id}, Access Token: ${access_token}`
+    ); // Log the success
 
-      // Handle successful authentication
-      res.send(`Access Token: ${access_token}, User ID: ${user_id}`);
+    // Handle successful authentication
+    res.send(`Access Token: ${access_token}, User ID: ${user_id}`);
   } catch (error) {
-      const errorMsg = error.response?.data || 'Unknown error';
-      logActivity(`Error exchanging code for token: ${JSON.stringify(errorMsg)}`); // Log the error
-      res.status(500).send('Error exchanging code for token.');
+    const errorMsg = error.response?.data || "Unknown error";
+    logActivity(`Error exchanging code for token: ${JSON.stringify(errorMsg)}`); // Log the error
+    res.status(500).send("Error exchanging code for token.");
   }
 };
-
 
 // // Start the OAuth authorization process
 // exports.startOAuth = (req, res) => {
